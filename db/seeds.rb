ad1e94b1f4bda688ea252d80e76e2db589f1317a
Weston Ellis <ellisweston112@gmail.com>
2021-08-26 13:42:22 -0600

    Add seed files

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+pine = Bookshelf.create!(name: 'Pine', full: false, capacity: 12)
+oak = Bookshelf.create!(name: 'Oak', full: true, capacity: 2)
+Book.create!(name: 'Feet of Clay', read: true, read_time: 6, bookshelf_id: oak.id)
+Book.create!(name: 'Three Guineas', read: false, read_time: 12, bookshelf_id: oak.id)
+
+bummer = CarLot.create!(name: "Michael'z Bummer Dealz", being_cleaned: false, lot_area: 16)
+fly = CarLot.create!(name: "Fly Rides", being_cleaned: true, lot_area: 9001)
+Vehicle.create!(name: 'Toyota Corolla', sold: true, price: 300, car_lot_id: fly.id)
+Vehicle.create!(name: 'Honda Civic', sold: false, price: 801, car_lot_id: fly.id)
