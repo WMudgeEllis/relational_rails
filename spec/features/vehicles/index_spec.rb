28ac8aa77b75a5345681186fc8c44dc9d24bd5ac
Weston Ellis <ellisweston112@gmail.com>
2021-08-26 18:06:28 -0600

    Finish story 6 for car_lot

--- spec/features/vehicles/index_spec.rb
+++ spec/features/vehicles/index_spec.rb
@@ -5,7 +5,7 @@ RSpec.describe 'vehicles index page' do
     fly_guys = CarLot.create!(name: "Fly Rydes", being_cleaned: true, lot_area: 1200)
     car1 = Vehicle.create!(name: 'Toyota Yaris', sold: true, price: 7500, car_lot_id: fly_guys.id)
     car2 = Vehicle.create!(name: 'Chevrolet Silverado', sold: true, price: 10000, car_lot_id: fly_guys.id)
-    
+
     visit '/vehicles'
 
     expect(page).to have_content(car1.name)
@@ -18,5 +18,6 @@ RSpec.describe 'vehicles index page' do
     expect(page).to have_content(car2.car_lot_id)
     expect(page).to have_content(car1.id)
     expect(page).to have_content(car2.id)
+
   end
 end
