1e8ca4515fc7216f90e198331d8f40616b0ddea7
Blake Dempsey <bdempsey864@gmail.com>
2021-08-28 15:12:02 -0600

    Finish story 11 functionality

--- app/controllers/car_lots_controller.rb
+++ app/controllers/car_lots_controller.rb
@@ -11,6 +11,10 @@ class CarLotsController < ApplicationController
   end
 
   def create
-    require "pry"; binding.pry
+    being_cleaned = params[:being_cleaned] == true
+    CarLot.create(name: params[:name], being_cleaned: being_cleaned, lot_area: params[:lot_area].to_i)
+    redirect_to "/car_lots"
   end
+
+
 end
