18a990f9770e8ca6b52d67d79088e7a1810a860b
Weston Ellis <ellisweston112@gmail.com>
2021-08-30 20:11:09 -0600

    Add user story 17 test

--- spec/features/bookshelves/index_spec.rb
+++ spec/features/bookshelves/index_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe 'the bookshelves index page', type: :feature do
     shelf2 = Bookshelf.create!(name: "Cherry Wood", full: true, capacity: 31, created_at: Date.tomorrow)
 
     visit '/bookshelves'
-    # require "pry"; binding.pry
+
     expect(page).to have_content(shelf1.name)
     expect(page).to have_content(shelf2.created_at)
     expect(page.all(:css, "p")[0].text.include?("Cherry")).to be(true)
@@ -30,4 +30,25 @@ RSpec.describe 'the bookshelves index page', type: :feature do
 
     expect(page).to have_content(shelf.name)
   end
+
+  it 'each shelf can link to edit page' do
+    shelf1 = Bookshelf.create!(name: "Mahogany", full: true, capacity: 31)
+    shelf2 = Bookshelf.create!(name: "Cherry Wood", full: true, capacity: 31, created_at: Date.tomorrow)
+
+    visit '/bookshelves'
+
+    expect(page).to have_link("edit #{shelf1.name}")
+    expect(page).to have_link("edit #{shelf2.name}")
+
+    click_link shelf1.name
+
+    expect(current_path).to eq("/bookshelves/#{shelf1.name}/edit")
+  end
+
 end
+#
+# As a visitor
+# When I visit the parent index page
+# Next to every parent, I see a link to edit that parent's info
+# When I click the link
+# I should be taken to that parents edit page where I can update its information just like in User Story 4
