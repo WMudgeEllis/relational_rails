e2ebdf3ce15f7d0f1d11a801284b7d8c9c79daa9
Blake Dempsey <bdempsey864@gmail.com>
2021-08-31 17:06:39 -0600

    Write test for user story 20

--- spec/features/vehicles/show_spec.rb
+++ spec/features/vehicles/show_spec.rb
@@ -22,4 +22,16 @@ RSpec.describe 'vehicle details show page' do
     click_link "update vehicle"
     expect(current_path).to eq("/vehicles/#{car.id}/edit")
   end
+
+  it 'deletes a vehicle' do
+    fly_guys = CarLot.create!(name: "Fly Rydes", being_cleaned: true, lot_area: 1200)
+    car1 = Vehicle.create!(name: 'Toyota Yaris', sold: true, price: 7500, car_lot_id: fly_guys.id)
+    car2 = Vehicle.create!(name: 'Toyota Taco', sold: true, price: 17500, car_lot_id: fly_guys.id)
+    visit "/vehicles/#{car1.id}"
+    expect(page).to have_link('delete vehicle')
+    click_link "delete vehicle"
+    expect(current_path).to eq("/vehicles")
+    expect(page).to_not have_content("Yaris")
+    expect(page).to have_content("Taco")
+  end
 end
